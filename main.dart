6d49562cfd02bd8cc3533bc91541948cff393dfa
anggoro <75475767+anggoroexe@users.noreply.github.com>
2023-11-06 09:58:00 +0700

    Create main.dart
    mobil mobilan

---
+++ main.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'home.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Push Named and Remove Until'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pushAndRemoveUntil(
+                  context,
+                  MaterialPageRoute(builder: (context) => HomeScreen()),
+                  (Route<dynamic> route) => false,
+                );
+              },
+              child: Text('Tampilkan Halaman Lain'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
